609f39e2c73fa40e73942f5ec55d40d63950d63a
itachi1706 <kennethsohyq@hotmail.com>
2014-12-07 23:01:22 +0800

    Testing out foreach

--- Testing/QueueTest/QueueTest/Main.cpp
+++ Testing/QueueTest/QueueTest/Main.cpp
@@ -2,8 +2,44 @@
 #include "Queue.h"
 
 using namespace std;
+
+int se(int arrayPassIn[]){
+	///int arrayPassIn[] = { 1, 5, 7, 8, 22, 34, 55, 77, 99 };
+	for (int num : arrayPassIn){
+
+	}
+}
+
+int search(int dataArray[], int arraySize, int start, int target){
+	//int arrayPassIn[] = { 1, 5, 7, 8, 22, 34, 55, 77, 99 };
+	start = 0;
+	for (int num : dataArray)
+	{
+		if (num == target){
+			return start;
+		}
+		else if (num > target){
+			return -1;
+		}
+		else {
+			start++;
+		}
+	}
+	return 0;
+}
+
+
 int main(){
-	Queue queue;
+	int dataArray[] = { 1, 2, 3, 4, 5 };
+
+	for (int i : dataArray)
+	{
+		cout << i << endl;
+	}
+
+	int arrayPassIn[] = { 1, 5, 7, 8, 22, 34, 55, 77, 99 };
+	cout << search(arrayPassIn, 9, 0, 22) << endl;
+	/*Queue queue;
 	if (queue.isEmpty())
 		cout << "Queue is Empty" << endl;
 	else
@@ -24,5 +60,5 @@ int main(){
 		cout << "Queue is Empty" << endl;
 	else
 		cout << "Queue is NOT Empty" << endl;
-	return 0;
+	return 0;*/
 }
