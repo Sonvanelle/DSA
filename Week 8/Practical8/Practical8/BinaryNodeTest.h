047d86bf632a13274f4a005d345e24f0e4630fd6
itachi1706 <kennethsohyq@hotmail.com>
2015-01-11 21:33:00 +0800

    Removed Test Code from Official Code

--- Week 8/Practical8/Practical8/BinaryNodeTest.h
+++
@@ -1,8 +0,0 @@
-typedef int ItemType;
-
-struct BinaryNode
-{
-	ItemType   item;		// data item
-	BinaryNode *left;	// pointer pointing to left subtree
-	BinaryNode *right;	// pointer pointing to right subtree
-};
